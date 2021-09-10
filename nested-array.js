45af059eadc687d01ff15a1a8b1f2548288c86a5
antoRados <antorados@gmail.com>
2021-09-10 00:58:32 -0400

    updated nested-array.js file, initial commit functionDrillsLab.js

--- nested-array.js
+++ nested-array.js
@@ -1,12 +1,5 @@
 const myArr = [[6], [2], [3], [7], [5]]
-/*var myArr = new Array ();
 
-myArr[0] = new Array ([1]);
-myArr[1] = new Array ([2]);
-myArr[2] = new Array ([3]);
-myArr[3] = new Array ([4]);
-myArr[4] = new Array ([5]);
-*/
 for(i = 0; i < myArr.length; i++) {
 	if(myArr[i] % 2 === 0){
 		console.log("Even")
